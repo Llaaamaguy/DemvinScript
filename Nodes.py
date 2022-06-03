0eed3c0a2d2998e82a5dabfd26cf8b99e414e045
Llaaamaguy <llamaguy.8267@gmail.com>
2022-06-03 17:15:14 -0400

    Initial commit

---
+++ Nodes.py
@@ -0,0 +1,67 @@
+class VarNode:
+    def __init__(self, varName, varValue):
+        self.varName = varName
+        self.varValue = varValue
+
+    def __repr__(self):
+        return str(self.varValue)
+
+    def __str__(self):
+        return str(self.varValue)
+
+
+class StrNode:
+    def __init__(self, string):
+        self.string = string
+
+    def __repr__(self):
+        return self.string
+
+    def __str__(self):
+        return self.string
+
+
+class IntNode:
+    def __init__(self, value):
+        self.value = int(value)
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def eval(self):
+        return self.value
+
+
+class FloatNode:
+    def __init__(self, value):
+        self.value = float(value)
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def eval(self):
+        return self.value
+
+
+class TimesNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self):
+        return self.left.eval() * self.right.eval()
+
+
+class PlusNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self):
+        return self.left.eval() + self.right.eval()
---
+++ interpreter.py
@@ -0,0 +1,69 @@
+from wit import Wit
+import os
+from Nodes import *
+
+token = os.environ["WIT_TOKEN"]
+client = Wit(token)
+
+
+def parse_value(value):
+    try:
+        int(value)
+    except ValueError:
+        return StrNode(value)
+    if "." in [x for x in value]:
+        return FloatNode(value)
+    else:
+        return IntNode(value)
+
+
+def main():
+    with open("code.txt", "r") as f:
+        lines = f.readlines()
+
+    varStore = {}
+
+    for line in lines:
+        if line.strip():
+            result = client.message(line)
+            linetype = result["intents"][0]["name"]
+            # print(f"{lines.index(line)}: {linetype}")
+            if linetype == "var_def":
+                try:
+                    varName = result["entities"]["var_name:var_name"][0]["body"]
+                    varVal = result["entities"]["var_value:var_value"][0]["body"]
+                    varStore[varName] = VarNode(varName, parse_value(varVal))
+                except KeyError:
+                    try:
+                        varName = result["entities"]["var_name:var_name"][0]["body"]
+                        eval_left = result["entities"]["eval_left:eval_left"][0]["body"]
+                        eval_right = result["entities"]["eval_right:eval_right"][0]["body"]
+                        eval_op = result["entities"]["eval_operator:eval_operator"][0]["body"]
+                        if eval_left in list(varStore.keys()):
+                            eval_left = str(varStore[eval_left].varValue)
+                        if eval_right in list(varStore.keys()):
+                            eval_right = str(varStore[eval_right].varValue)
+                        if eval_op == "+":
+                            varStore[varName] = VarNode(varName, PlusNode(parse_value(eval_left), parse_value(eval_right)).eval())
+                        elif eval_op == "*":
+                            varStore[varName] = VarNode(varName, TimesNode(parse_value(eval_left), parse_value(eval_right)).eval())
+                    except KeyError:
+                        print(f"WARNING ON {line}: All entities not found\nIgnoring variable definition")
+                    pass
+            elif linetype == "output":
+                try:
+                    toPrint = result["entities"]["out_val:out_val"][0]["body"]
+                    if toPrint in list(varStore.keys()):
+                        toPrint = str(varStore[toPrint])
+                    print(f"SYS-OUT: {toPrint}")
+                except KeyError:
+                    print(f"WARNING ON {line}: All entities not found\nIgnoring output statement")
+        else:
+            # print(f"{lines.index(line)}: Empty line")
+            pass
+
+    # print(varStore)
+
+
+if __name__ == "__main__":
+    main()
